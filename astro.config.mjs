bbf1a57aad955052004df53e741e6e2bdef181ee
Miguel Rivas <mrivaslezma@gmail.com>
2025-01-24 21:56:32 -0300

    adding vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
+import tailwindcss from "@tailwindcss/vite";
+
 
 export default defineConfig({
   site: 'https://mrivaslezama.github.io',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(),],
+  plugins: [tailwindcss()],
   base: '/postreshelados',
+  output:'server',
+  adapter: vercel(),
   build: {
     rollupOptions: {
       external: ['bootstrap/js/dist/collapse.js'],
